a3f4c3ded7beb41240fffbf0d8363c0ef832dc59
tinpt1 <tinpham101@gmail.com>
2021-05-19 11:34:43 +0700

    sstream example

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,24 @@
 #include <iostream>
-#include <chrono>
-#include <cstdlib>
-#include <ctime>
 #include <string>
+#include <array>
+#include <iomanip>
+#include <sstream>
 
-class Timer
-{
-private:
-	using clock_t = std::chrono::high_resolution_clock;
-	using second_t = std::chrono::duration<double, std::ratio<1>>;
-	std::chrono::time_point<clock_t> m_beg;
-public:
-	Timer() : m_beg{clock_t::now()}
-	{}
-
-	void reset()
-	{
-		m_beg = clock_t::now();
-	}
-
-	double elapse() const
-	{
-		return std::chrono::duration_cast<second_t>(clock_t::now() - m_beg).count();
-	}
-};
 
 int main()
 {
-	Timer t;
+	std::stringstream os;
+	std::string string1{"123"};
+	std::string string2{"123.456"};
+	int intNumber{};
+	double doubleNumber{};
 
-	std::srand(std::time(nullptr));
-	std::string s;
-	s.reserve(64);
-	for(int i{0}; i < 64; ++i)
-	{
-		s += 'a' + std::rand() % 26;
-	}
-	std::cout << s << std::endl;
+	os << string1 << ' ' << string2;
 
-	std::cout << "Time remaining is " << t.elapse() << " second\n";
+	os >> intNumber >> doubleNumber;
+
+	std::cout << intNumber << ' ' << doubleNumber;
+
 
 	return 0;
 }
